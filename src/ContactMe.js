0e524f42774a46f19232b091a8bdccd01b0e003f
Tommy <tommy128works@gmail.com>
2023-06-04 06:28:18 -0400

    feat: add contact me section with responsiveness
    Added contact me section with dummy content and implemented
    responsive design for desktop, tablet, and mobile viewing.

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -1,5 +1,25 @@
 function ContactMe() {
-  return <div>CONTACT ME -- Nothing implemented.</div>;
+  return (
+    <div className="contact-me">
+      <span>
+        <h1>Contact Me</h1>
+        <div>
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+          ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+          egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+          eleifend leo.
+        </div>
+      </span>
+      <span>
+        <img
+          className="contact-me-image"
+          src={require("./images/placeholder_profile.jpg")}
+          alt="Tommy Tran"
+        />
+      </span>
+    </div>
+  );
 }
 
 export default ContactMe;
