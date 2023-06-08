4ee32a3cede479e309af9c7fb62696526bea3675
Tommy <tommy128works@gmail.com>
2023-06-08 08:09:23 -0400

    feat: add progress to theme creation
    Tried new background images, but reached the conclusion that a plain
    colour background would be cleaner and more professional.
    A concept of section containers was created.

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,23 +1,25 @@
 function AboutMe() {
   return (
     <div className="about-me">
-      <span>
+      <div>
         <img
           className="about-me-image"
           src={require("./images/placeholder_profile.jpg")}
           alt="Tommy Tran"
         />
-      </span>
-      <span>
-        <h1>About Me</h1>
-        <div>
+      </div>
+      <div className="about-me-desc">
+        <h1 className="highlight">Hi there!</h1>
+        <h1 className="highlight">I'm Tommy Tran</h1>
+        <p>
+          TEST
           Pellentesque habitant morbi tristique senectus et netus et malesuada
           fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
           ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
           egestas semper. Aenean ultricies mi vitae est. Mauris placerat
           eleifend leo.
-        </div>
-      </span>
+        </p>
+      </div>
     </div>
   );
 }
